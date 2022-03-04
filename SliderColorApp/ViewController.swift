d52af38f03c6a4ea0d74cc0c20cd2e326d812e8d
auhustsinovich <avgustinovich4@gmail.com>
2022-03-04 22:13:22 +0100

    Constrains updated

--- SliderColorApp/ViewController.swift
+++ SliderColorApp/ViewController.swift
@@ -15,22 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSliderValue: UILabel!
     @IBOutlet weak var blueSliderValue: UILabel!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greenSliderValue.text = "542"
         colorView.layer.cornerRadius = 25
     }
     
     @IBAction func redSliderValueChanged() {
+        redSliderValue.text = String(round(redSlider.value * 100) / 100)
         
     }
     @IBAction func greenSliderValueChanged() {
+        greenSliderValue.text = String(round(greenSlider.value * 100) / 100)
         
     }
     @IBAction func blueSliderValueChanged() {
-        
+        blueSliderValue.text = String(round(blueSlider.value * 100) / 100)
     }
-
 }
 
